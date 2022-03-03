da9e91e9475fe9574161d00ec9fae00dae34899b
Fikcup <rhys.n.wright@gmail.com>
2022-03-02 18:15:35 -0800

    feat : home page start

--- client/views/Home.js
+++ client/views/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 const Home = () => {
     return (
         <View>
-            
+            {/* TODO: add in logo here */}
+            <Text>Bev-Wiz</Text>
+
+            {/* TODO: add subtext/catch phrase */}
+
+            <Button>Let's Drink</Button>
         </View>
     );
 }
+
+// TODO: Add style sheet
 
 export default Home;
