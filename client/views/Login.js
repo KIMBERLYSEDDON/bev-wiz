010b4b9c76d183f4a024e5385c23a9f3ab0715e7
Fikcup <rhys.n.wright@gmail.com>
2022-03-02 18:24:46 -0800

    feat : form shell

--- client/views/Login.js
+++ client/views/Login.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Form, TextInput } from 'react-native';
 
 const Login = () => {
+    // TODO: add state changes for form inputs
+
     return (
         <View>
-            
+            {/* TODO: Add BevWiz logo component */}
+            <Text>Login</Text>
+
+            <Form>
+                <TextInput 
+                    name="username"
+                    placeholder="username"
+                />
+                <TextInput 
+                    name="password"
+                    placeholder="password"
+                    secureTextEntry={true}
+                />
+            </Form>
         </View>
     );
 }
+
+// TODO: Add style sheet
 
 export default Login;
