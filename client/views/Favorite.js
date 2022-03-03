26fec781149d48f8ee140e09b2cfe61f86ca21b8
Fikcup <rhys.n.wright@gmail.com>
2022-03-02 18:12:53 -0800

    docs : view initial files

---
+++ client/views/Favorite.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Favorite = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default Favorite;
---
+++ client/views/Location.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Location = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default Location;
