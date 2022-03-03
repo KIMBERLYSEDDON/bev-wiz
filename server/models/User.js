57dd46ece2d8b90184cd05b02410a03054a0c39b
KIMBERLYSEDDON <82526676+KIMBERLYSEDDON@users.noreply.github.com>
2022-03-02 19:55:49 -0600

    Users model

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    }
+});
+
+// hook to compare and validate password for logging in
+userSchema.pre("save", async function (next) {
+    if (this.isNew || this.isModified("password")) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// hook to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+
+const User = model("User", userSchema);
+
+module.exports = User;
