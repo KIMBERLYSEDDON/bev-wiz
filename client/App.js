ce3a727feaa6742c22960e655101aa16f428df2a
Kevin Devlin <azwethinkweizkd@gmail.com>
2022-04-16 13:09:31 -0600

    Completed basic card, needs accurate flex box set up for dynamically displaying for different phones, installed new dependencies

--- client/App.js
+++ client/App.js
@@ -1,60 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { useAsyncStorage } from '@react-native-async-storage/async-storage';
-
-// // Storing Object Value
-// const storeData = async (value) => {
-//   try {
-//     const jsonValue = JSON.stringify(value)
-//     await AsyncStorage.setItem('@storage_Key', jsonValue)
-//   } catch (e) {
-//     // saving error
-//   }
-// }
-
-// const USER_1 = {
-//   name: 'Alena',
-//   age: 29,
-// };
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import Home from "./src/views/Home";
 
 export default function App() {
-  const [value, setValue] = useState('value');
-  const { getItem, setItem } = useAsyncStorage('@storage_key');
-
-  const readItemFromStorage = async () => {
-    const item = await getItem();
-    setValue(item);
-  };
-
-  const writeItemToStorage = async newValue => {
-    await setItem(newValue);
-    setValue(newValue);
-  };
-
-  useEffect(() => {
-    readItemFromStorage();
-  }, []);
-
-  return (
-    <View style={{ margin: 40 }}>
-      <Text>Current value: {value}</Text>
-      <TouchableOpacity
-        onPress={() =>
-          writeItemToStorage(Math.random().toString(36).substr(2, 5))
-        }
-      >
-        <Text>Update value</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <Home />;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
