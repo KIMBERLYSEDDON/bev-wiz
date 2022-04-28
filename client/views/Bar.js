87db3465d1f4211d4a2206b6102ce3052269f70f
KIMBERLYSEDDON <82526676+KIMBERLYSEDDON@users.noreply.github.com>
2022-04-27 19:36:57 -0500

    new file

---
+++ client/views/Bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Bar = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+// TODO: Add style sheet
+
+export default Bar;
