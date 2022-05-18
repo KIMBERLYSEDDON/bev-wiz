3364da33caa3b0d52f3930b0da19c1cf0c387e26
Fikcup <rhys.n.wright@gmail.com>
2022-05-17 21:32:19 -0700

    fix: delete favorite improved and production html serve

--- server/controllers/favorite-controller.js
+++ server/controllers/favorite-controller.js
@@ -3,61 +3,76 @@ const jwt_decode = require("jwt-decode");
 
 const favoriteController = {
     async addFavorite(req, res) {
-        const { authorization } = req.headers;
-        const { id } = jwt_decode(authorization);
-
-        if (!id) {
-            throw new Error('User must be signed in.');
+        try {
+            const { authorization } = req.headers;
+            const { id } = jwt_decode(authorization);
+    
+            if (!id) {
+                throw new Error('User must be signed in.');
+            }
+    
+            const { favoriteId } = req.body;
+            if (!favoriteId) {
+                throw new Error('Missing required fields.');
+            }
+    
+            const favoriteObj = {
+                favoriteId: favoriteId.trim(),
+                userId: id,
+            };
+    
+            const favorite = new Favorite(favoriteObj);
+            await favorite.save();
+    
+            if (!favorite) {
+                throw new Error('Invalid input.');
+            }
+    
+            res.send(favorite);
+        } catch (err) {
+            res.status(500).json(err);
         }
-
-        const { favoriteId } = req.body;
-        if (!favoriteId) {
-            throw new Error('Missing required fields.');
-        }
-
-        const favoriteObj = {
-            favoriteId: favoriteId.trim(),
-            userId: id,
-        };
-
-        const favorite = new Favorite(favoriteObj);
-        await favorite.save();
-
-        if (!favorite) {
-            throw new Error('Invalid input.');
-        }
-
-        res.send(favorite);
     },
     async removeFavorite(req, res) {
-        const { authorization } = req.headers;
-        const { id } = jwt_decode(authorization);
-
-        if (!id) {
-            throw new Error('User must be signed in.');
+        try {
+            const { authorization } = req.headers;
+            const { id } = jwt_decode(authorization);
+    
+            if (!id) {
+                throw new Error('User must be signed in.');
+            }
+    
+            const { _id } = req.body;
+            const favorite = await Favorite.findByIdAndRemove(_id);
+            if (!favorite) {
+                throw new Error('No valid favorite provided.');
+            }
+    
+            res.send("success");
+        } catch (err) {
+            res.status(500).json(err);
         }
-
-        const { _id } = req.params;
-        const favorite = await Favorite.findByIdAndRemove(_id);
-        if (!favorite) {
-            throw new Error('No valid favorite provided.');
-        }
-
-        res.send("success");
     },
     async getMyFavorites(req, res) {
-        const { authorization } = req.headers;
-        const { id } = jwt_decode(authorization);
+        try {
+            const { authorization } = req.headers;
+            const { id } = jwt_decode(authorization);
 
-        if (!id) {
-            throw new Error('User must be signed in.');
+            console.log(id);
+    
+            if (!id) {
+                throw new Error('User must be signed in.');
+            }
+    
+            const favorites = await Favorite.find({
+                where: { userId: id },
+            });
+            console.log(favorites);
+    
+            res.send({ favorites: favorites });
+        } catch (err) {
+            res.status(500).json(err);
         }
-
-        const favorites = await Favorite.findAll({
-            where: { userId: id },
-        });
-
-        res.send({ favorites: favorites });
     },
 };
 
--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('/', (_req, res) => {
+        res.sendFile(path.join(__dirname, '../client/'));
+    });
+}
+
 app.use(routes);
 
 db.once('open', () => {
