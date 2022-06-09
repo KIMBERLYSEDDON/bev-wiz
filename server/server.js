c19cc3460c240dae78e4ca2479031189b298e5cb
Kevin Devlin <azwethinkweizkd@gmail.com>
2022-06-08 19:11:53 -0600

    added changes to match original kevin branch

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 
+const db = require("./config/connection");
+const routes = require("./routes");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
